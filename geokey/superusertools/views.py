a8e059c459f74a88b1464e2464454b29782e5c4b
Julius Osokinas <j.osokinas@mappingforchange.org.uk>
2016-07-27 17:53:23 +0100

    Provider overview page, where it can be activated and updated

--- geokey/superusertools/views.py
+++ geokey/superusertools/views.py
@@ -223,16 +223,101 @@ class ProviderList(LoginRequiredMixin, SuperuserMixin, TemplateView):
         return {'providers': providers.registry.get_list()}
 
 
-class ProviderSettings(LoginRequiredMixin, SuperuserMixin, TemplateView):
-    """Settings of a provider page."""
+class ProviderContext(LoginRequiredMixin, SuperuserMixin):
+    """Context mixin that adds a provider to render the template."""
 
-    template_name = 'superusertools/provider_settings.html'
+    def get_context_data(self, provider_id, *args, **kwargs):
+        """
+        Return the context to render the view.
+
+        Add a provider and optional social app to the context.
+
+        Parameters
+        ----------
+        provider_id : str
+            Identifies the provider in the list of available providers.
+
+        Returns
+        -------
+        dict
+        """
+        try:
+            provider = providers.registry.by_id(provider_id)
+
+            try:
+                social_app = SocialApp.objects.get_current(
+                    provider.id,
+                    self.request
+                )
+            except SocialApp.DoesNotExist:
+                social_app = None
+
+            return super(ProviderContext, self).get_context_data(
+                provider=provider,
+                social_app=social_app,
+                *args,
+                **kwargs
+            )
+        except:
+            return {
+                'error': 'Not found.',
+                'error_description': 'Provider not found.'
+            }
 
 
-class ProviderDelete(LoginRequiredMixin, SuperuserMixin, TemplateView):
-    """Delete a provider."""
+class ProviderOverview(ProviderContext, TemplateView):
+    """Overview of a provider page."""
 
-    template_name = 'base.html'
+    template_name = 'superusertools/provider_overview.html'
+
+    def post(self, request, provider_id):
+        """
+        Handle POST request.
+
+        Update or enable the social app.
+
+        Parameters
+        ----------
+        request : django.http.HttpRequest
+            Object representing the request.
+        provider_id : str
+            Identifies the provider in the list of available providers.
+
+        Returns
+        -------
+        django.http.HttpResponse
+        """
+        data = request.POST
+        context = self.get_context_data(provider_id)
+        provider = context.get('provider')
+        social_app = context.get('social_app')
+
+        if social_app:
+            social_app.client_id = data.get('client_id')
+            social_app.secret = data.get('secret')
+            social_app.key = data.get('key')
+            social_app.save()
+            messages.success(
+                self.request,
+                'Provider has been updated.'
+            )
+        else:
+            social_app = SocialApp.objects.create(
+                provider=provider.id,
+                name=provider.name,
+                client_id=data.get('client_id'),
+                secret=data.get('secret'),
+                key=data.get('key')
+            )
+            social_app.sites.add(get_current_site(request))
+            messages.success(
+                self.request,
+                'Provider has been activated.'
+            )
+
+        context['social_app'] = social_app
+
+        return self.render_to_response(context)
 
 
 # #############################################################################
